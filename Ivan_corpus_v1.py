ed6ec4d72737b1eb04b7d7260ab829210a169341
Ivanhandsome777 <83528914+Ivanhandsome777@users.noreply.github.com>
2023-04-09 21:49:26 +0800

    Update Ivan_corpus_v1.py

--- Ivan_corpus_v1.py
+++ Ivan_corpus_v1.py
@@ -4,7 +4,7 @@ import math
 
 
 class IvanCorpus:
-    corpus_content_default = pd.read_parquet("./version1_corpus.parquet")
+    corpus_content_default = pd.read_parquet("/Users/densoushin/Desktop/上财大四下/Japanese_anime_corpus/version1_corpus.parquet")
     def __init__(self,corpus_content=corpus_content_default):
         self.corpus_content = corpus_content
         self.shape = corpus_content.shape
@@ -34,28 +34,34 @@ class IvanCorpus:
         word_scale = self.words_num
         list_df=[]
         for word in words_list:
-            CHR_count = int(df_word_frequency.query("""word ==@word_search""")['word_frequency'])
-            CHR_million = np.round(CHR_count/(word_scale/1000000),digit)
-            log_CHR = np.round(math.log10(CHR_count),digit)
-            CHR_CD = int(df_word_frequency.query("""word ==@word_search""").n_anime)
-            CHR_CD_per = np.round(CHR_CD/anime_scale,digit)
-            log_CHR_CD = np.round(math.log10(CHR_CD),digit)
-            dict_value = {
-                "word":word,
-                "CHR_count":CHR_count,
-                "CHR_million":CHR_million,
-                "log_CHR":log_CHR,
-                "CHR_CD":CHR_CD,
-                "CHR_CD_per":CHR_CD_per,
-                "log_CHR_CD":log_CHR_CD
-            }
-            df_unit = pd.DataFrame(dict_value)
-            list_df.append(df_unit)
+            try:
+                CHR_count = int(df_word_frequency.query("""word ==@word""")['word_frequency'])
+                CHR_million = np.round(CHR_count/(word_scale/1000000),digit)
+                log_CHR = np.round(math.log10(CHR_count),digit)
+                CHR_CD = int(df_word_frequency.query("""word ==@word""").n_anime)
+                CHR_CD_per = np.round(CHR_CD/anime_scale,digit)
+                log_CHR_CD = np.round(math.log10(CHR_CD),digit)
+                dict_value = {
+                    "word":word,
+                    "CHR_count":CHR_count,
+                    "CHR_million":CHR_million,
+                    "log_CHR":log_CHR,
+                    "CHR_CD":CHR_CD,
+                    "CHR_CD_per":CHR_CD_per,
+                    "log_CHR_CD":log_CHR_CD
+                }
+                df_unit = pd.DataFrame(dict_value,index=[0])
+                list_df.append(df_unit)
+            except:
+                print(word,"is not in the corpus")
         out_put_df = pd.concat(list_df)
         if export_to_excel is True:
             out_put_df.to_excel(export_path+export_file_name)
             print("successfully export",export_path,export_file_name)
         else:
             return out_put_df
+    
+    
+
     
     
